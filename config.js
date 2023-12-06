65988d79076b8cd5c4b6496319215477196fbe65
Msmklucas <lucasalmeida.pf008@academico.ifsul.edu.br>
2023-12-06 19:40:09 -0300

    alteracao config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ let pool = null;
 
 if (isProduction){
     pool = new Pool({
-        connectionString : process.env.DATABASE_URL, 
+        connectionString : process.env.DATABASE_URL 
     })
 } else {
     pool = new Pool({
